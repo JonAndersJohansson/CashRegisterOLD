6c5af399cb51bd86cb11950aa69219ecd5c2850c
Jon Johansson <jonjohansson88@gmail.com>
2024-10-03 17:18:11 +0200

    Första pushen.

--- Kassasystem/Program.cs
+++ Kassasystem/Program.cs
@@ -7,45 +7,43 @@
             RegisterMethods.MainMenu();     // Börjar med att hämta MENY
 
 
-        //    bool validInput = false;
-        //    while (!validInput)
-        //    {
-        //        Console.Clear();  // Rensar konsollen
-        //        DisplayMenu();    // Skriv ut menyn igen efter rensningen
+            //    bool validInput = false;
+            //    while (!validInput)
+            //    {
+            //        Console.Clear();  // Rensar konsollen
+            //        DisplayMenu();    // Skriv ut menyn igen efter rensningen
 
-        //        string input = Console.ReadLine();
+            //        string input = Console.ReadLine();
 
-        //        switch (input)
-        //        {
-        //            case "1":
-        //                Console.WriteLine("Du valde alternativ 1.");
-        //                validInput = true;
-        //                break;
-        //            case "2":
-        //                Console.WriteLine("Du valde alternativ 2.");
-        //                validInput = true;
-        //                break;
-        //            case "3":
-        //                Console.WriteLine("Avslutar...");
-        //                validInput = true;
-        //                break;
-        //            default:
-        //                Console.WriteLine("Ogiltigt alternativ, försök igen.");
-        //                System.Threading.Thread.Sleep(2000);  // Pausar kort innan vi rensar
-        //                break;
-        //        }
-        //    }
-        //}
-        //public static void DisplayMenu()
-        //{
-        //    Console.WriteLine("Välkommen till huvudmenyn");
-        //    Console.WriteLine("1. Alternativ 1");
-        //    Console.WriteLine("2. Alternativ 2");
-        //    Console.WriteLine("3. Avsluta");
-        //    Console.WriteLine("Välj ett alternativ: ");
-        //}
-
-
+            //        switch (input)
+            //        {
+            //            case "1":
+            //                Console.WriteLine("Du valde alternativ 1.");
+            //                validInput = true;
+            //                break;
+            //            case "2":
+            //                Console.WriteLine("Du valde alternativ 2.");
+            //                validInput = true;
+            //                break;
+            //            case "3":
+            //                Console.WriteLine("Avslutar...");
+            //                validInput = true;
+            //                break;
+            //            default:
+            //                Console.WriteLine("Ogiltigt alternativ, försök igen.");
+            //                System.Threading.Thread.Sleep(2000);  // Pausar kort innan vi rensar
+            //                break;
+            //        }
+            //    }
+            //}
+            //public static void DisplayMenu()
+            //{
+            //    Console.WriteLine("Välkommen till huvudmenyn");
+            //    Console.WriteLine("1. Alternativ 1");
+            //    Console.WriteLine("2. Alternativ 2");
+            //    Console.WriteLine("3. Avsluta");
+            //    Console.WriteLine("Välj ett alternativ: ");
+            //}
 
 
 
@@ -62,5 +60,8 @@
 
 
 
+
+
+        }
     }
 }
